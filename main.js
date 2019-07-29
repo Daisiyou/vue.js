1e4e10569336b0d529395346a9ea2450dabae1b1
daisiyou <490903692@qq.com>
2019-07-29 23:54:09 +0800

    add style images

--- main.js
+++ main.js
@@ -2,7 +2,20 @@ var app = new Vue({
     el:'#app',
     devtools:true,
     data:{
-        product:'Socks'
+        product:'Socks',
+        image:'images/socksBlue.jpg',
+        inStock:true,
+        details:["80% cotton", "20% polyester", "Gender-neutral"],
+        variants:[
+            {
+                variantId:2334,
+                variantColor:"green"
+            },
+            {
+                variantId:2335,
+                variantColor:"blue"
+            },
+        ]
     }
 })
 
